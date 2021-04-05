281a0deccca81c20d4aba8a29f181ac04fa7dcdc
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-05 15:34:50 +0500

    int selialize object

--- ConcurencyTask/StringExtensions.cs
+++ ConcurencyTask/StringExtensions.cs
@@ -1,17 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Text.Json;
 
 namespace ConcurencyTask
 {
     public static class StringExtensions
     {
-        public static string ToJsonString<T>(this string str, T obj)
+        public static string ToJson<T>(this string str, T obj)
         {
             return JsonSerializer.Serialize<T>(obj);
+        }
+        
+        public static int ToInt32(this string str)
+        {
+            var result = new Int32();
+            if(Int32.TryParse(str, out result))
+            {
+                return result;
+            }
+
+            return -1;
         }
     }
 }
