474804d783bc1b719a35adf00a444a50ce50dc4a
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-05 15:02:26 +0500

    string extension method for serializing

---
+++ ConcurencyTask/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace ConcurencyTask
+{
+    public static class StringExtensions
+    {
+        public static string ToJsonString<T>(this string str, T obj)
+        {
+            return JsonSerializer.Serialize<T>(obj);
+        }
+    }
+}
