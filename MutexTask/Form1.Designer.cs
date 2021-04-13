a5f03a1c655840c904d7fbb66541a08f5b666a9d
ulugbekabirov@gmail.com <ulugbekabirov@gmail.com>
2021-04-13 13:13:09 +0500

    mutex task setup

---
+++ MutexTask/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace MutexTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProgressBar1 = new System.Windows.Forms.ProgressBar();
+            this.ProgressBar2 = new System.Windows.Forms.ProgressBar();
+            this.Start = new System.Windows.Forms.Button();
+            this.Switch = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ProgressBar1
+            // 
+            this.ProgressBar1.Location = new System.Drawing.Point(35, 60);
+            this.ProgressBar1.Name = "ProgressBar1";
+            this.ProgressBar1.Size = new System.Drawing.Size(708, 29);
+            this.ProgressBar1.TabIndex = 0;
+            this.ProgressBar1.Click += new System.EventHandler(this.ProgressBar1_Click);
+            // 
+            // ProgressBar2
+            // 
+            this.ProgressBar2.Location = new System.Drawing.Point(35, 123);
+            this.ProgressBar2.Name = "ProgressBar2";
+            this.ProgressBar2.Size = new System.Drawing.Size(708, 29);
+            this.ProgressBar2.TabIndex = 1;
+            this.ProgressBar2.Click += new System.EventHandler(this.ProgressBar2_Click);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(35, 227);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(94, 29);
+            this.Start.TabIndex = 2;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Switch
+            // 
+            this.Switch.Location = new System.Drawing.Point(356, 227);
+            this.Switch.Name = "Switch";
+            this.Switch.Size = new System.Drawing.Size(94, 29);
+            this.Switch.TabIndex = 3;
+            this.Switch.Text = "Switch";
+            this.Switch.UseVisualStyleBackColor = true;
+            this.Switch.Click += new System.EventHandler(this.Switch_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(649, 227);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(94, 29);
+            this.Stop.TabIndex = 4;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Switch);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.ProgressBar2);
+            this.Controls.Add(this.ProgressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar ProgressBar1;
+        private System.Windows.Forms.ProgressBar ProgressBar2;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Switch;
+        private System.Windows.Forms.Button Stop;
+    }
+}
+
---
+++ MutexTask/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MutexTask
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ProgressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ProgressBar2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Switch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
